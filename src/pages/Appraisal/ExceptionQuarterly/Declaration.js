54f5acd6980c4d3d9560ffb327d2ed3c1e39dc05
shivangi <106944056+Shivangi889@users.noreply.github.com>
2025-11-15 17:48:50 +0530

    selectedbutton fixed

--- src/pages/Appraisal/ExceptionQuarterly/Declaration.js
+++ src/pages/Appraisal/ExceptionQuarterly/Declaration.js
@@ -90,29 +90,55 @@ function DeclarationSection() {
 
       {/* File Upload + Submit Button */}
       <div className="file-upload-bar d-flex align-items-center justify-content-between gap-3 p-2 rounded">
-        <div className="d-flex align-items-start flex-row">
-          <label
-            htmlFor="fileInput"
-           className="btn px-4"
-          style={{
-            backgroundColor: "var(--accent-color)",
-            color: "#fff",
-            fontWeight: 500,
-            whiteSpace: "nowrap",
-          }}
-          >
-            Select a File
-          </label>
-          <input
-            id="fileInput"
-            type="file"
-            accept=".jpg,.jpeg,.pdf"
-            onChange={handleFileChange}
-            disabled={!isChecked}
-            className="form-control"
-            style={{ flexGrow: 1 }}
-          />
-        </div>
+         <div className="d-flex align-items-center flex-grow-1">
+
+    {/* Green custom button */}
+    <label
+      htmlFor="fileInput"
+      className="btn px-4"
+      style={{
+        backgroundColor: "var(--accent-color)",
+        color: "#fff",
+        fontWeight: 500,
+        whiteSpace: "nowrap",
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+      }}
+    >
+      Select a File
+    </label>
+
+    {/* HIDDEN REAL FILE INPUT */}
+    <input
+      id="fileInput"
+      type="file"
+      accept=".jpg,.jpeg,.pdf"
+      onChange={handleFileChange}
+      disabled={!isChecked}
+      style={{ display: "none" }}
+    />
+
+    {/* File name display box */}
+    <div
+      className="px-3 py-2"
+      style={{
+        border: "1px solid #ccc",
+        borderLeft: "none",
+        width: "16rem",
+        background: "#fff",
+        height: "40px",
+        display: "flex",
+        alignItems: "center",
+        color: "#333",
+        fontSize: "14px",
+        borderTopRightRadius: "4px",
+        borderBottomRightRadius: "4px",
+      }}
+    >
+      {file ? file.name : "No file selected"}
+    </div>
+
+  </div>
 
         <button
           className="btn px-4"
